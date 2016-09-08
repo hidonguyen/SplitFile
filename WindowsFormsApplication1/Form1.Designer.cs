f1c13663f377ed43566869ef54fae6b9e15638f0
nhthinh@tma.com.vn <nhthinh@tma.com.vn>
2016-09-08 09:19:49 +0700

    Init Project

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBrowsFile = new System.Windows.Forms.TextBox();
+            this.btn_Brows = new System.Windows.Forms.Button();
+            this.btn_splitFile = new System.Windows.Forms.Button();
+            this.btn_mertFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblSendingResult = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtBrowsFile
+            // 
+            this.txtBrowsFile.Location = new System.Drawing.Point(72, 74);
+            this.txtBrowsFile.Name = "txtBrowsFile";
+            this.txtBrowsFile.Size = new System.Drawing.Size(227, 20);
+            this.txtBrowsFile.TabIndex = 0;
+            // 
+            // btn_Brows
+            // 
+            this.btn_Brows.Location = new System.Drawing.Point(319, 73);
+            this.btn_Brows.Name = "btn_Brows";
+            this.btn_Brows.Size = new System.Drawing.Size(75, 23);
+            this.btn_Brows.TabIndex = 1;
+            this.btn_Brows.Text = "Brows";
+            this.btn_Brows.UseVisualStyleBackColor = true;
+            this.btn_Brows.Click += new System.EventHandler(this.brows_Click);
+            // 
+            // btn_splitFile
+            // 
+            this.btn_splitFile.Location = new System.Drawing.Point(414, 73);
+            this.btn_splitFile.Name = "btn_splitFile";
+            this.btn_splitFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_splitFile.TabIndex = 1;
+            this.btn_splitFile.Text = "Split file";
+            this.btn_splitFile.UseVisualStyleBackColor = true;
+            this.btn_splitFile.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // btn_mertFile
+            // 
+            this.btn_mertFile.Location = new System.Drawing.Point(244, 254);
+            this.btn_mertFile.Name = "btn_mertFile";
+            this.btn_mertFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_mertFile.TabIndex = 1;
+            this.btn_mertFile.Text = "Merge File";
+            this.btn_mertFile.UseVisualStyleBackColor = true;
+            this.btn_mertFile.Click += new System.EventHandler(this.btnMergeFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblSendingResult
+            // 
+            this.lblSendingResult.AutoSize = true;
+            this.lblSendingResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSendingResult.Location = new System.Drawing.Point(159, 18);
+            this.lblSendingResult.Name = "lblSendingResult";
+            this.lblSendingResult.Size = new System.Drawing.Size(259, 25);
+            this.lblSendingResult.TabIndex = 2;
+            this.lblSendingResult.Text = "Split Files and merge files";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(90, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(379, 134);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 289);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblSendingResult);
+            this.Controls.Add(this.btn_mertFile);
+            this.Controls.Add(this.btn_splitFile);
+            this.Controls.Add(this.btn_Brows);
+            this.Controls.Add(this.txtBrowsFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBrowsFile;
+        private System.Windows.Forms.Button btn_Brows;
+        private System.Windows.Forms.Button btn_splitFile;
+        private System.Windows.Forms.Button btn_mertFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblSendingResult;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public FileStream Fs;
+        string _mergeFolder;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> Packets = new List<string>();
+        //Merge file is stored in drive
+        string _saveFileFolder = @"c:\";
+        readonly int _nNoofFiles = 20;
+        //Code Under Brows button:                  
+        private void brows_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.ShowDialog();
+                txtBrowsFile.Text = openFileDialog1.FileName;
+                Fs = new FileStream(txtBrowsFile.Text, FileMode.Open, FileAccess.Read);
+                int fileLength = (int)Fs.Length / 1024;
+                string name = Path.GetFileName(txtBrowsFile.Text);
+            }
+            catch (Exception ex)
+            {
+                lblSendingResult.Text = "EXCEPTION:" + ex;
+            }
+        }
+        //Code Under Split button:
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            SplitFile(txtBrowsFile.Text, Convert.ToInt32(_nNoofFiles));
+            for (int i = 0; i < _nNoofFiles; i++)
+            {
+                listBox1.Items.Add(Packets[i]);
+            }
+        }
+        public bool SplitFile(string sourceFile, int nNoofFiles)
+        {
+            try
+            {
+                FileStream fs = new FileStream(sourceFile, FileMode.Open, FileAccess.Read);
+                int sizeofEachFile = (int)Math.Ceiling((double)fs.Length / nNoofFiles);
+                for (int i = 0; i < nNoofFiles; i++)
+                {
+                    string baseFileName = Path.GetFileNameWithoutExtension(sourceFile);
+                    string extension = Path.GetExtension(sourceFile);
+                    FileStream outputFile = new FileStream(Path.GetDirectoryName(sourceFile) + "\\" + baseFileName + "." +
+                        i.ToString().PadLeft(5, Convert.ToChar("0")) + extension + ".tmp", FileMode.Create, FileAccess.Write);
+                    _mergeFolder = Path.GetDirectoryName(sourceFile);
+                    int bytesRead = 0;
+                    byte[] buffer = new byte[sizeofEachFile];
+                    if ((bytesRead = fs.Read(buffer, 0, sizeofEachFile)) > 0)
+                    {
+                        outputFile.Write(buffer, 0, bytesRead);
+                        //outp.Write(buffer, 0, BytesRead);
+                        string packet = baseFileName + "." + i.ToString().PadLeft(3, Convert.ToChar("0")) + extension;
+                        Packets.Add(packet);
+                    }
+                    outputFile.Close();
+                }
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+
+            return false;
+        }
+        //Code Under Merge button:
+        //Files have been merged and saved at location C:\\"
+        private void btnMergeFile_Click(object sender, EventArgs e)
+        {
+            if (_mergeFolder == null)
+            {
+                _mergeFolder = Path.GetDirectoryName(txtBrowsFile.Text);
+            }
+            MergeFile(_mergeFolder);
+        }
+        public bool MergeFile(string inputfoldername1)
+        {
+            try
+            {
+                string[] tmpfiles = Directory.GetFiles(inputfoldername1, "*.tmp");
+                FileStream outPutFile = null;
+                string prevFileName = "";
+                foreach (string tempFile in tmpfiles)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(tempFile);
+                    if (fileName == null)
+                    {
+                        throw new Exception("File name is invalid!");
+                    }
+                    string baseFileName = fileName.Substring(0, fileName.IndexOf(Convert.ToChar(".")));
+                    string extension = Path.GetExtension(fileName);
+                    if (!prevFileName.Equals(baseFileName))
+                    {
+                        if (outPutFile != null)
+                        {
+                            outPutFile.Flush();
+                            outPutFile.Close();
+                        }
+                        outPutFile = new FileStream(inputfoldername1 + "\\" + baseFileName + extension, FileMode.OpenOrCreate, FileAccess.Write);
+                    }
+                    int bytesRead = 0;
+                    byte[] buffer = new byte[1024];
+                    FileStream inputTempFile = new FileStream(tempFile, FileMode.OpenOrCreate, FileAccess.Read);
+                    while ((bytesRead = inputTempFile.Read(buffer, 0, 1024)) > 0)
+                        outPutFile.Write(buffer, 0, bytesRead);
+                    inputTempFile.Close();
+                    //File.Delete(tempFile);
+                    prevFileName = baseFileName;
+                }
+                outPutFile.Close();
+                lblSendingResult.Text = "Files have been merged and saved at location C:\\";
+                return true;
+            }
+            catch(Exception ex)
+            {
+                // ignored
+            }
+            return false;
+        }
+    }
+}
